72ffcc393227a57d2223e09568e137576541c5ae
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-07-22 16:59:15 -0700

    fetch api data
    - used usedEffect Hook'
    - then update states with usedState

--- moviedb/src/components/Home.js
+++ moviedb/src/components/Home.js
@@ -1,4 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+//api
+
+import API from '../API';
 
 //config 
 //importing things from config file 
@@ -18,7 +21,38 @@ const Home = () => {
     //before useState hook you need to create a class that was stated in React
     const [error, setError] = useState(false); //use if receive error from API
 
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
 
+            const movies = await API.fetchMovies(searchTerm, page);
+            console.log(movies);
+            //now that you have movies set the state
+            //return an object
+            setState(prev => (
+                {
+                    ...movies,
+                    results: 
+                        page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+                }
+            ));
+            setLoading(false);
+        } catch(error){
+            //set the state
+            setError(true);
+        }
+    };
+
+    //useEffect called with inline function
+    //trigger this only when we mount the component 
+    //can do it by specifying , []
+    //when you specify it as an empty array it will onyl run once 
+
+    //inital render
+    useEffect( () => {
+        fetchMovies(1);
+    }, [])
     return <div>Home Page</div>
 };
 
