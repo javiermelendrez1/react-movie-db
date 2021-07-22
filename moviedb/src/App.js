3a18a2ee4694546ebdd2a9fb9a692633bb8852e6
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-07-22 16:08:24 -0700

    created header component
    - also styled the header component

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+//components
+import Header from './components/Header';
 //styles
 import {GlobalStyle} from './GlobalStyle';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       Start here.
       <GlobalStyle />
     </div>
