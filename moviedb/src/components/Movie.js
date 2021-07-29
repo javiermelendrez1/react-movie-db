2b4449601fb61e821f3ed391086c2b0524f7626b
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-07-29 12:15:15 -0700

    created hook for fetching movie detail

--- moviedb/src/components/Movie.js
+++ moviedb/src/components/Movie.js
@@ -1,5 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+//config 
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+//componets
+import Grid from './Grid';
+import Spinner from './Spinner';
+//hook 
+import { useMovieFetch } from '../hooks/useMovieFetch';
+//image
+import NoImage from '../images/no_image.jpg';
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
-const Movie = () => <div>Movie</div>;
+const Movie = () => {
+
+    const {movieId} = useParams();
+
+    const [state, loading, error] = useHomeFetch(movieId);
+    return (
+        <>
+        <div>Movie</div>
+        </>
+    )
+};
 
 export default Movie;
